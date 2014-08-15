62b0fe2a8439ce1ed661c4cb629eab1441ece835
YoungjaeKim <youngjae.kim@gmail.com>
2014-08-15 23:02:06 +0900

    초기 화면에 버전명 표기.

--- project/src/main/java/net/tagboa/app/page/SplashActivity.java
+++ project/src/main/java/net/tagboa/app/page/SplashActivity.java
@@ -2,9 +2,12 @@ package net.tagboa.app.page;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.widget.TextView;
 import net.tagboa.app.R;
 
 /**
@@ -14,20 +17,42 @@ import net.tagboa.app.R;
 public class SplashActivity extends Activity {
 	// http://stackoverflow.com/questions/15452061/android-splash-screen?rq=1
 	private static long SLEEP_TIME = 1000;    // Sleep for some time
+    private android.widget.TextView textViewBottom;
+    private String appVersion;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
 
-		setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash);
+        this.textViewBottom = (TextView) findViewById(R.id.textViewBottom);
 
-		// Start timer and launch main activity
-		IntentLauncher launcher = new IntentLauncher();
-		launcher.start();
-	}
+        // 버전명 표시.
+        textViewBottom.setText("version " + getAppVersionName());
 
-	/**
+        // Start timer and launch main activity
+        IntentLauncher launcher = new IntentLauncher();
+        launcher.start();
+    }
+
+    /**
+     * 버전 가져오기.
+     * @return Android versionName
+     */
+    public String getAppVersionName() {
+        PackageInfo pi;
+        try {
+            pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "0.0";
+        }
+
+        return pi.versionName;
+    }
+
+    /**
 	 * 갤러리 앱에서 실행될 시.
 	 */
 	private class IntentLauncher extends Thread {
